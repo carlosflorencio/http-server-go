466e411fa52b16a33ad83b3e7dc3e2c7e5adbfc3
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-10-22 22:13:49 +0100

    add basic request parsing

--- app/server.go
+++ app/server.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
+	"net/textproto"
 	"os"
+	"strings"
 )
 
 func main() {
-
 	listener, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -31,12 +34,54 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-	_, err := conn.Read(buf)
+	req, err := convertRequest(conn)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		fmt.Println("Error converting request: ", err.Error())
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if req.path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
+
+type request struct {
+	proto   string
+	method  string
+	path    string
+	body    []byte
+	headers textproto.MIMEHeader
+}
+
+func convertRequest(conn net.Conn) (*request, error) {
+	reader := bufio.NewReader(conn)
+	text := textproto.NewReader(reader)
+
+	req := new(request)
+
+	// parse first line, GET / HTTP/1.1
+	line, err := text.ReadLine()
+	if err != nil {
+		fmt.Println("Error parsing first line: ", err.Error())
+		return nil, err
+	}
+
+	parts := strings.Split(line, " ")
+	req.method = parts[0]
+	req.path = parts[1]
+	req.proto = parts[2]
+
+	// parse headers
+	req.headers, err = text.ReadMIMEHeader()
+	if err != nil {
+		fmt.Println("Error parsing headers: ", err.Error())
+		return nil, err
+	}
+
+	if req.method == "GET" || req.method == "HEAD" {
+		return req, nil
+	}
+
+	return nil, errors.New("Unsupported method: " + req.method)
 }

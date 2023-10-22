1110228937b9529f5452d2130d56eb7d9afe7a27
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-10-22 21:24:34 +0100

    add read

--- app/server.go
+++ app/server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -30,6 +30,13 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
